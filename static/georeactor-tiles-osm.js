15fa4d1d39325b73239576eea324d53b084b8244
Nick Doiron <ndoiron@mapmeld.com>
2015-12-28 16:50:03 +0530

    button is more helpful: zoom in / edit osm

--- static/georeactor-tiles-osm.js
+++ static/georeactor-tiles-osm.js
@@ -1,12 +1,25 @@
 function initTileView() {
   $("#loading").hide();
-  var editButton = $("<button class='btn'>Edit OSM</button>").click(function() {
+  var warning = $("<p>Are these roads missing? Zoom in to check, then click Edit OSM.</p>");
+  $("#sidebar").append(warning);
+
+  var editButton = $("<button class='btn'>Zoom In</button>").click(function() {
     var center = map.getCenter();
     var zoom = map.getZoom();
     if (zoom < 16) {
-      return alert("Zoom in for more detail before opening the OSM editor");
+      return map.zoomIn();
     }
     window.location.href = "http://www.openstreetmap.org/edit?editor=id#map=" + [Math.round(zoom), center.lat, center.lng].join("/") + "&background=custom:https://b.tiles.mapbox.com/v4/planemad.ba506c30/{z}/{x}/{y}.png%3Faccess_token=pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ";
   });
+  map.on('zoomstart', function() {
+    editButton.text("...");
+  }).on('zoomend', function() {
+    if (map.getZoom() >= 16) {
+      editButton.text("Edit OSM");
+    } else {
+      editButton.text("Zoom In");
+    }
+  });
+
   $("#sidebar").append(editButton);
 }
